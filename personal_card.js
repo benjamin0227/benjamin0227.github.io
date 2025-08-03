f3cc5b8c55dd4ec3e826068a9402d2df519035f3
Mingyuan Jia <2601286601@qq.com>
2025-08-03 17:58:15 +0800

    Add files via upload

---
+++ personal_card.js
@@ -0,0 +1,16 @@
+
+// 获取dom元素
+const btn = document.getElementById('sayHigh')
+const input = document.getElementById('nameInput')
+
+// 事件监听
+// 元素.addEventListener('事件名称', 回调函数）
+btn.addEventListener('click', function() {
+    const name = input.value.trim();//去掉首尾空格
+
+    const who = name || 'friend';
+
+    // 模板字符串要用反引号！
+    alert(`Hello, ${who}!`);
+    document.title = `你好, ${who}`;
+});
